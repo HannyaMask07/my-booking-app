52b36b359e9dc906307a0ba0d082c53bf5ebb176
HannyaMask07 <krzysztof.trytek07@gmail.com>
2024-07-12 11:39:42 +0200

    adding log packages

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
+import * as dotenv from "dotenv";
+dotenv.config();
 import express, { application } from "express";
 const app = express();
+import morgan from "morgan";
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 app.use(express.json());
 
@@ -11,6 +18,9 @@ app.post("/", (req, res) => {
   console.log(req);
   res.json({ message: "data received", data: req.body });
 });
-app.listen(5100, () => {
-  console.log("server running...");
+
+const port = process.env.PORT || 5100;
+
+app.listen(port, () => {
+  console.log(`server running on PORT ${port}...`);
 });
