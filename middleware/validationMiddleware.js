bcb005531533722effc8dda3742ae2efca4cc8b3
HannyaMask07 <krzysztof.trytek07@gmail.com>
2024-07-12 23:20:31 +0200

    Authorization changes

--- middleware/validationMiddleware.js
+++ middleware/validationMiddleware.js
@@ -2,6 +2,7 @@ import { body, param, validationResult } from "express-validator";
 import { BadRequestError, NotFoundError } from "../errors/customErrors.js";
 import mongoose from "mongoose";
 import Desk from "../models/DeskModel.js";
+import User from "../models/UserModel.js";
 
 const withValidationErrors = (validateValues) => {
   return [
@@ -31,4 +32,36 @@ export const validateIdParam = withValidationErrors([
     const desk = await Desk.findById(value);
     if (!desk) throw new NotFoundError(`no desk with id ${value}`);
   }),
+]);
+
+export const validateRegisterInput = withValidationErrors([
+  body("name").notEmpty().withMessage("name is required"),
+  body("email")
+    .notEmpty()
+    .withMessage("email is required")
+    .isEmail()
+    .withMessage("invalid email format")
+    .custom(async (email) => {
+      const user = await User.findOne({ email });
+      if (user) {
+        throw new BadRequestError("email already exist");
+      }
+    }),
+  body("password")
+    .notEmpty()
+    .withMessage("password is required")
+    .isLength({ min: 4 })
+    .withMessage("password must be at least 4 characters long"),
+  body("location").notEmpty().withMessage("location is required"),
+  body("lastName").notEmpty().withMessage("last name is required"),
+]);
+
+export const validateLoginInput = withValidationErrors([
+  body("email")
+    .notEmpty()
+    .withMessage("email is required")
+    .isEmail()
+    .withMessage("invalid email format"),
+
+  body("password").notEmpty().withMessage("password is required"),
 ]);
