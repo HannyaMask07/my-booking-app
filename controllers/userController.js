154141756bc09d2b28669d92b1158ba127bc226b
HannyaMask07 <krzysztof.trytek07@gmail.com>
2024-07-13 17:32:43 +0200

    Creating new routes for users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,21 @@
 import { StatusCodes } from "http-status-codes";
 import User from "../models/UserModel.js";
+import Desk from "../models/DeskModel.js";
 
 export const getCurrentUser = async (req, res) => {
-  res.status(StatusCodes.OK).json({ msg: "get current user" });
+  const user = await User.findOne({ _id: req.user.userId });
+  const userWithoutPassword = user.toJSON();
+  res.status(StatusCodes.OK).json({ userWithoutPassword });
 };
 export const getApplicationStats = async (req, res) => {
-  res.status(StatusCodes.OK).json({ msg: "application stats" });
+  const users = await User.countDocuments();
+  const desks = await Desk.countDocuments();
+  res.status(StatusCodes.OK).json({ users, desks });
 };
 export const updateUser = async (req, res) => {
-  res.status(StatusCodes.OK).json({ msg: "update user" });
+  const obj = { ...req.body };
+  delete obj.password;
+
+  const updatedUser = await User.findByIdAndUpdate(req.user.userId, obj);
+  res.status(StatusCodes.OK).json("updated user");
 };
